47c1af4cd0efdd7b9f8cc2a8d1e22c703fa36bb5
Ali Samii <a.samii@gmail.com>
2017-05-03 14:22:27 -0500

    Adding problem 2.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "solved_problems/problems/diffusion/tokamak_anisotropy.hpp"
+#include "solved_problems/problems/diffusion/tokamak_anisotropy_2.hpp"
 
 //
 //
@@ -12,6 +12,6 @@
 int main(int argc, char **argv)
 {
   //
-  Problem1<3>::run(argc, argv);
+  Problem2<3>::run(argc, argv);
   //
 }
